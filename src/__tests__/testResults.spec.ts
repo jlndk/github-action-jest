cbc1bc2f15514b81fc1dbf49bd2f032b07bdea55
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-02-01 22:40:24 +0100

    Add more tests

---
+++ src/__tests__/testResults.spec.ts
@@ -0,0 +1,88 @@
+import { FormattedTestResults } from '@jest/test-result/build/types';
+import { startGroup, endGroup } from '@actions/core';
+import { issueCommand } from '@actions/core/lib/command';
+import { reportTestResults, printAnnotation } from '../testResults';
+
+jest.mock('@actions/core');
+jest.mock('@actions/core/lib/command');
+
+describe('reportTestResults', () => {
+  it('does nothing if numFailedTests is zero', () => {
+    reportTestResults({
+      numFailedTests: 0,
+    } as FormattedTestResults);
+
+    expect(startGroup).not.toBeCalled();
+    expect(issueCommand).toBeCalledTimes(0);
+    expect(endGroup).not.toBeCalled();
+  });
+
+  it('prints failureMessages', () => {
+    const results = {
+      numFailedTests: 1,
+      testResults: [
+        {
+          assertionResults: [{ failureMessages: ['(foobar.js:42:69)this is a failure'] }],
+        },
+      ],
+    } as FormattedTestResults;
+
+    reportTestResults(results);
+
+    expect(startGroup).toBeCalledWith('Jest Annotations');
+    expect(issueCommand).toBeCalledTimes(1);
+    expect(endGroup).toBeCalled();
+  });
+
+  it('skips printing message if it does not match regex', () => {
+    const results = {
+      numFailedTests: 1,
+      testResults: [
+        {
+          assertionResults: [{ failureMessages: ['this does not match regex'] }],
+        },
+      ],
+    } as FormattedTestResults;
+
+    reportTestResults(results);
+
+    expect(startGroup).toBeCalledWith('Jest Annotations');
+    expect(issueCommand).not.toBeCalled();
+    expect(endGroup).toBeCalled();
+  });
+
+  it('skips printing message failureMessages does not exist', () => {
+    const results = {
+      numFailedTests: 1,
+      testResults: [{ assertionResults: [{ failureMessages: null }] }],
+    } as FormattedTestResults;
+
+    reportTestResults(results);
+
+    expect(startGroup).toBeCalledWith('Jest Annotations');
+    expect(issueCommand).not.toBeCalled();
+    expect(endGroup).toBeCalled();
+  });
+});
+
+describe('printAnnotation', () => {
+  it('parses and prints message', () => {
+    const msg = '(foobar.js:42:69)this is a failure';
+
+    printAnnotation(msg);
+
+    expect(issueCommand).toBeCalledWith(
+      'error',
+      { file: 'foobar.js', line: '42', col: '69' },
+      '(foobar.js:42:69)this is a failure'
+    );
+  });
+
+  it('skips printing message if it does not match regex', () => {
+    const msg = 'this does not match regex';
+
+    printAnnotation(msg);
+
+    expect(issueCommand).not.toBeCalled();
+  });
+});
--- src/testResults.ts
+++ src/testResults.ts
@@ -1,4 +1,4 @@
-import type { FormattedTestResults } from '@jest/test-result/build/types';
+import { FormattedTestResults } from '@jest/test-result/build/types';
 import { startGroup, endGroup } from '@actions/core';
 import { issueCommand } from '@actions/core/lib/command';
 
@@ -11,22 +11,26 @@ export function reportTestResults(result: FormattedTestResults): void {
 
   startGroup('Jest Annotations');
 
-  for (const testResult of result.testResults) {
-    for (const assertion of testResult.assertionResults) {
-      assertion?.failureMessages?.forEach((msg) => {
-        const match = regex.exec(msg);
-        if (match && match.length > 2) {
-          const args = {
-            file: match[1],
-            line: match[2],
-            col: match[3],
-          };
-
-          issueCommand('error', args, msg);
-        }
-      });
-    }
-  }
+  result.testResults
+    // Merge the deeply nested array of objects into a simple string array
+    .flatMap((t) => t.assertionResults.flatMap((a) => a.failureMessages))
+    // Remove empty/non-existant messages
+    .filter((msg): msg is string => msg != null)
+    // Print all messages
+    .forEach((msg) => printAnnotation(msg));
 
   endGroup();
+}
+
+export function printAnnotation(msg: string): void {
+  const match = regex.exec(msg);
+  if (match && match.length > 2) {
+    const args = {
+      file: match[1],
+      line: match[2],
+      col: match[3],
+    };
+
+    issueCommand('error', args, msg);
+  }
 }
