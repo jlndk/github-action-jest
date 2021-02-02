0bf8deebc69dd967c13abaaa33544bd411b4259e
Jonas Lindenskov Nielsen <lindenskov.nielsen@gmail.com>
2021-02-02 11:43:49 +0100

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import path, { sep } from 'path';
 import * as core from '@actions/core';
-import { getGithubToken, getBooleanArg } from './args';
+import { FormattedTestResults } from '@jest/test-result/build/types';
+import { getGithubToken, getBooleanArg, getCWD } from './args';
 import { generateCommentBody } from './coverage';
 import updateOrCreateComment from './comment';
 import runJest, { getJestCommand, readTestResults } from './run';
@@ -8,16 +9,39 @@ import { reportTestResults } from './testResults';
 
 async function main(): Promise<void> {
   // Get args
-  const baseCommand = core.getInput('test-command', { required: false }) ?? 'npm test';
-  const workingDirectory = core.getInput('working-directory', { required: false });
   const shouldCommentCoverage = getBooleanArg('coverage-comment');
-  const dryRun = getBooleanArg('dry-run');
-  const runOnlyChangedFiles = getBooleanArg('changes-only');
-  const exitOnJestFail = getBooleanArg('fail-action-if-jest-fails');
+  const cwd = getCWD();
 
   // Compute paths
-  const cwd = workingDirectory ? path.resolve(workingDirectory) : process.cwd();
   const coverageFilePath = path.join(cwd + sep, 'jest.results.json');
+
+  // Run jest and read the results file
+  core.info('Executing jest');
+  const statusCode = await executeJest(coverageFilePath, cwd, shouldCommentCoverage);
+  const results = readTestResults(coverageFilePath);
+
+  // Parse the result file, output annotations, and exit if tests failed
+  core.info('Reporting test results');
+  reportTestResults(results);
+  exitIfFailed(statusCode);
+
+  // Return early if we should not post code coverage comment
+  if (!shouldCommentCoverage) {
+    core.info('Code coverage commenting is disabled. Skipping...');
+    return;
+  }
+
+  // Generate table for coverage data and output it
+  await outputCoverageTable(results);
+}
+
+async function executeJest(
+  coverageFilePath: string,
+  cwd: string,
+  shouldCommentCoverage: boolean
+): Promise<number> {
+  const baseCommand = core.getInput('test-command', { required: false }) ?? 'npm test';
+  const runOnlyChangedFiles = getBooleanArg('changes-only');
 
   // Make the jest command
   const cmd = getJestCommand({
@@ -27,50 +51,48 @@ async function main(): Promise<void> {
     withCoverage: shouldCommentCoverage,
   });
 
-  core.info('Executing jest');
+  return await runJest({ cmd, cwd });
+}
 
-  // execute jest
-  const statusCode = await runJest({ cmd, cwd });
-  const results = readTestResults(coverageFilePath);
-
-  core.info('Reporting test results');
-  reportTestResults(results);
+function exitIfFailed(statusCode: number): void {
+  const exitOnJestFail = getBooleanArg('fail-action-if-jest-fails');
 
   if (exitOnJestFail && statusCode !== 0) {
     throw new Error(
       'Jest returned non-zero exit code. Check annotations or debug output for more information.'
     );
-  } else if (!exitOnJestFail) {
+  }
+
+  // Output status message if "fail-action-if-jest-fails" is disabled
+  if (!exitOnJestFail) {
     core.info(
       'Continuing even though jest failed, since "fail-action-if-jest-fails" is false.'
     );
   }
+}
 
-  // Return early if we should not post code coverage comment
-  if (!shouldCommentCoverage) {
-    core.info('Code coverage commenting is disabled. Skipping...');
-    return;
-  }
-
-  const coverageMap = results.coverageMap;
+async function outputCoverageTable({ coverageMap }: FormattedTestResults): Promise<void> {
+  const dryRun = getBooleanArg('dry-run');
 
   if (!coverageMap) {
     throw new Error('Could not find coverage info in jest results file');
   }
 
   core.info('Generating coverage table');
+
   // Make the comment content
   const commentContent = generateCommentBody(coverageMap);
 
   if (dryRun) {
     core.debug(`Dry run detected: Here's the content of the comment:`);
     core.debug(commentContent);
-  } else {
-    core.info('Posting comment');
-    const githubToken = getGithubToken();
-    // Post the comment.
-    await updateOrCreateComment(commentContent, githubToken);
+    return;
   }
+
+  core.info('Posting comment');
+  const githubToken = getGithubToken();
+  // Post the comment.
+  await updateOrCreateComment(commentContent, githubToken);
 }
 
 main().catch((err) => {
